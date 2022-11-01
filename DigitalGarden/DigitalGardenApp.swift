67d12f0670eb9adb70427327af43f89b6f09e7ae
Spencer Rafada <spencer.byui@gmail.com>
2022-10-31 20:15:03 -0600

    Initial Commit

---
+++ DigitalGarden/DigitalGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalGardenApp.swift
+//  DigitalGarden
+//
+//  Created by Spencer Rafada on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct DigitalGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
